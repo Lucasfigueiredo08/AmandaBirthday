458eccc77830ce6e8a1946971fb85b306009cea4
Lucas Figueiredo <lukas.liberato14@gmail.com>
2020-06-30 01:16:17 -0300

    musiquinha

--- src/Main/index.js
+++ src/Main/index.js
@@ -6,14 +6,24 @@ import image from './../assets/happybirthday.png';
 
 import Galeria from '../Components/Galeria';
 
+import ReactAudioPlayer from 'react-audio-player';
+
+import song from './parabens.ogg';
+
 export default class Main extends Component {
     render() {
         return (
             <Container>
+                <ReactAudioPlayer
+                    src={song}
+                    autoPlay
+                />
                 <ImageBirthDay src={image}/>
-                <Text>Baby, te desejo muitas alegrias, felicidades, saúde, que você possa alcançar tudo o que você deseja e que seja sempre alegre. Te gosto muito! <strong> S2  </strong></Text>
+                <Text>Baby, te desejo muitas alegrias, felicidades, saúde, que você possa alcançar tudo o que você deseja e que seja sempre alegre. Te gosto muito!
+                     <strong> S2  </strong></Text>
                 
                 <Galeria />
+                
             </Container>
         );
     }
